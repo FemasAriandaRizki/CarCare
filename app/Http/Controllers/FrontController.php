37f358168fbce6341917e96816a5fd3805061dae
FemasAriandaRizki <human.earth1606@gmail.com>
2025-01-13 01:11:24 +0700

    integrasi halaman home (2)

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CarService;
+use App\Models\City;
 use Illuminate\Http\Request;
 
 class FrontController extends Controller
 {
     //
     public function index(){
-        return view('front.index');
+        $cities = City::all();
+        $services = CarService::withCount(['storeServices'])->get();
+
+        return view('front.index', compact('cities', 'services'));
     }
 }
